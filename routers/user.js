46ff7c146aed6cd4e65fe5da83ab5f7f9cb2eace
Zqy <zqy0412@gmail.com>
2024-05-19 23:59:44 +0800

    Add updatePubKey updateTaskStatus API

--- routers/user.js
+++ routers/user.js
@@ -129,5 +129,117 @@ router.post('/users/:id', async (req, res) => {
         res.status(500).json({error: 'Internal server error'});
     }
 })
+
+/**
+ * @swagger
+ * /users/updatePubKey/{id}:
+ *   post:
+ *     summary: Update user public key
+ *     description: Update a user with public key.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               pubKey:
+ *                 type: string
+ *                 description: The public key of the user
+ *             required:
+ *               - id
+ *               - pubKey
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The ID of the created user
+ *                 pubKey:
+ *                   type: string
+ *                   description: The public key of the user
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/users/updatePubKey/:id', async (req, res) => {
+    console.log(req.body);
+    const {pubKey} = req.body;
+    const id = req.params.id;
+    try {
+        const newUser = await prisma.user.update({
+            where: {id},
+            data: {
+                pubKey,
+            },
+        });
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
+
+/**
+ * @swagger
+ * /users/updateTaskStatus/{id}:
+ *   post:
+ *     summary: Update user public key
+ *     description: Update a user with public key.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               taskStatus:
+ *                 type: string
+ *                 description: The public key of the user
+ *             required:
+ *               - id
+ *               - taskStatus
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The ID of the created user
+ *                 taskStatus:
+ *                   type: string
+ *                   description: The taskStatus of the user
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/users/updateTaskStatus/:id', async (req, res) => {
+    console.log(req.body);
+    const {taskStatus} = req.body;
+    const id = req.params.id;
+    try {
+        const newUser = await prisma.user.update({
+            where: {id},
+            data: {
+                taskStatus,
+            },
+        });
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
 module.exports = router;
 
